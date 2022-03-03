5f4d9e4e622e15046d66d50cf61ed3ba48036c29
Steve Crass <stevecrass@Saras-MBP.hsd1.ut.comcast.net>
2022-03-03 16:10:33 -0700

    db CRUD complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
+  namespace :api do
+    get 'authors', to: 'authors#index'
+    post 'authors', to: 'authors#create'
+    get 'authors/:id', to: 'authors#show'
+    put 'authors/:id', to: 'authors#update'
+    delete 'authors/:id', to: 'authors#destroy'
+  end
 end
